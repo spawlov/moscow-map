3b300ea8500f999b9d601e01d24c3fed4b18737e
spawlov <spawlov@gmail.com>
2023-01-15 23:01:49 +0700

    Add load_place command

--- MoscowMap/settings.py
+++ MoscowMap/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/places/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'places/assets'),
+    os.path.join(BASE_DIR, 'assets'),
 )
 
 MEDIA_URL = '/media/'
