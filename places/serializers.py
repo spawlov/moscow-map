af1d7cf37e67e841fafa71bf41fa62c7d2a1ea30
spawlov <spawlov@gmail.com>
2023-01-18 13:38:55 +0700

    Error correction - add docstrings and comments

--- places/serializers.py
+++ places/serializers.py
@@ -6,20 +6,20 @@ from .models import Place
 
 
 class CordSerializer(serializers.BaseSerializer, ABC):
-    """Serializer coordinates for to places"""
+    """Serializer coordinates for to places for coordinates variable"""
     def to_representation(self, value):
         return {'lng': value.lng, 'lat': value.lat}
 
 
 class ImageSerializer(serializers.BaseSerializer, ABC):
-    """Serializer list of images for to places"""
+    """Serializer list of images for to places for imgs variable"""
     def to_representation(self, value):
         return f'{str(value.file.url)}'
 
 
 class PlaceSerializer(serializers.ModelSerializer):
-    coordinates = CordSerializer(source='*')
-    imgs = ImageSerializer(source='img_places', many=True)
+    coordinates = CordSerializer(source='*')  # Cords place
+    imgs = ImageSerializer(source='img_places', many=True)  # Image list
 
     class Meta:
         model = Place
